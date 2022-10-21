f8bfb99bde4d920716d05f26f7d589c394b40b32
MikeJamesWork <111097090+MikeJamesWork@users.noreply.github.com>
2022-10-21 15:19:29 +1000

    Add files via upload

---
+++ Homework.ps1
@@ -0,0 +1,32 @@
+﻿$HomeworkFolder = 'H:\Homework'
+#$Filename = "Something"
+$FileValue = 0
+$Files = "Zero Files"
+
+#$Files = Get-ChildItem $HomeworkFolder
+  Get-ChildItem $HomeworkFolder | ForEach-Object {
+  
+      $Contents = Get-Content $_.FullName
+       # $FileValue = Get-Content $_.$Files
+   
+         #$FileValue = Get-Content H:\Homework\TestFile1.txt
+           If ($Contents -eq 1)
+           {
+       
+             Remove-Item -LiteralPath $_.FullName
+             #$_.FullName | Remove-Item
+             # Write-Output $Contents
+            
+           }
+           #Elseif ($FileValue -ne 1)
+           Else
+          {
+           Write-Output "Not 1"
+          }
+}
+        
+        
+$var = Get-ChildItem $HomeworkFolder | Select-Object *,@{
+    Name = "Contents";
+    Expression = { Get-Content $_.FullName }
+} | Where-Object Contents -eq 1 | Remove-Item
